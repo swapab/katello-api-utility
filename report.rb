f6156bfdb2a86d2e2a5ed07caad3cd7260b28bd4
Swapnil Abnave <sabnave@redhat.com>
2016-10-12 15:04:35 -0400

    Refactor and a bit optimization

--- report.rb
+++ report.rb
@@ -6,13 +6,20 @@ require 'rest-client'
 require 'pry'
 require 'csv'
 
-username,password,hostname = ARGV
+puts "========= Generating CSV ========"
+
+username,password,hostname,filepath = ARGV
 
 hostname = hostname || Socket.gethostname
+
+FILEPATH = filepath || "~/reports.csv"
 
 puts "Username: #{username}"
 puts "Password: #{password}"
 puts "Hostname: #{hostname}"
+puts "Filepath: #{File.expand_path(FILEPATH)}"
+
+puts "=> Computing... Hold on"
 
 class Report
   attr_accessor :username, :password, :hostname, :api_caller
@@ -25,22 +32,21 @@ class Report
   end
 
   def generate
-    hosts = JSON.parse(all_hosts)["results"]
+    hosts = get_all_hosts
     host_ids = hosts.map{|host| host["id"]}
 
     final_csv = CSV.generate do |csv|
       csv << ["Hostname", "IP", "Packages"]
       hosts.each do |host|
-        puts host["id"]
         csv << [host["name"], host["ip"], enabled_products_per_host(host["id"])]
       end
     end
 
-    File.open(File.expand_path('~/reports.csv'), 'w:UTF-8') { |file| file.write(final_csv) }
+    File.open(File.expand_path(FILEPATH), 'w:UTF-8') { |file| file.write(final_csv) }
   end
 
   def enabled_products_per_host(host_id)
-    product_content = JSON.parse(api_caller.get("/api/v2/hosts/#{host_id}/subscriptions/product_content"))["results"]
+    product_content = get_product_content_per_host(host_id)
 
     product_content.inject([]) { |filtered_content_array, result_hash|
       filtered_content_array << result_hash["content"]["name"] if result_hash["enabled"]
@@ -50,34 +56,29 @@ class Report
     ""
   end
 
-  def all_content_views
-    api_caller.get("/katello/api/content_views")
-  end
-
-  def all_hosts
+  def get_all_hosts
     api_caller.get("/api/hosts")
   end
 
-  def package_per_host(host_id)
-    api_caller.get("/api/hosts/#{host_id}/packages")
+  def get_product_content_per_host(host_id)
+    api_caller.get("/api/v2/hosts/#{host_id}/subscriptions/product_content")
   end
 end
 
 class ApiCaller < Struct.new(:username, :password, :hostname)
-  attr_accessor :http_client
-
-  def initialize(username, password, hostname)
-    @http_client = RestClient::Request
-    super
+  def http_client
+    RestClient::Request
   end
 
   def get(path)
-    http_client.execute(
-      method: :get,
-      url: parse(path),
-      user: username,
-      password: password,
-      verify_ssl: false)
+    JSON.parse(
+      http_client.execute(
+        method: :get,
+        url: parse(path),
+        user: username,
+        password: password,
+        verify_ssl: false)
+    )["results"]
   end
 
   def parse(path)
@@ -87,3 +88,5 @@ end
 
 report = Report.new(username, password, hostname)
 report.generate
+
+puts "========= Done ======== | File saved : #{FILEPATH}"
